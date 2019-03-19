1adaadfdaf9f23e9d0b257a87c0962d64627f60b
Connor Payne <connorpayne@Connors-MBP.SpeedportEntry2090126250024>
2019-03-19 21:07:34 +0100

    fixed issues with web3 injection

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from "react";
 import {GameCanvas} from "./GameCanvas";
-// import EtherDriveContract from "./contracts/EtherWheel.json";
-// import getWeb3 from "./utils/getWeb3";
-import "./App.css";
+import EtherDriveContract from "./contracts/EtherWheel.json";
+import getWeb3 from "./utils/getWeb3";
+// import "./App.css";
+
 
 class App extends Component {
+  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = EtherDriveContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        EtherDriveContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+      // Set web3, accounts, and contract to the state, and then proceed with an
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
 
   render() {
     return (
       <div className="App">
-        <GameCanvas/>
+        <GameCanvas />
         <h1>Good to Go!</h1>
         <h2>Smart Contract Example</h2>
-
       </div>
     );
   }
